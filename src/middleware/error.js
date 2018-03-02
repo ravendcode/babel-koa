de3080af3a6762f118109134cd323a600e5bba41
ravendcode <ravendcode@gmail.com>
2018-03-02 17:20:05 +0200

    Fix

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -2,35 +2,15 @@ export default async (ctx, next) => {
   try {
     await next();
   } catch (err) {
-    const result = {};
-    result.message = err.message;
-    result.statusCode = err.statusCode || 500;
-    switch (result.statusCode) {
-      case 400:
-        result.error = 'Bad Request';
-        break;
-      case 401:
-        result.error = 'Unauthorized';
-        break;
-      case 403:
-        result.error = 'Forbidden';
-        break;
-      case 404:
-        result.error = 'Not Found';
-        break;
-      case 500:
-        result.error = 'Internal Server Error';
-        break;
-      default:
-        result.error = 'Internal Server Error';
-        break;
+    const result = {
+      statusCode: err.statusCode || 500,
+      error: err.name,
+    };
+    if (err.message) {
+      result.message = err.message;
     }
     ctx.status = result.statusCode;
-    ctx.body = {
-      statusCode: result.statusCode,
-      error: result.error,
-      message: result.message,
-    };
+    ctx.body = {...result};
     ctx.app.emit('error', err, ctx);
   }
 };
