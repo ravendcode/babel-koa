4c6d2037024e1918437a961701247733b28d545f
ravendcode <ravendcode@gmail.com>
2018-03-09 12:24:23 +0200

    Add jwt state

--- src/api/user/user.handler.js
+++ src/api/user/user.handler.js
@@ -3,23 +3,24 @@ export default {
     if (!id) {
       ctx.throw(404, 'Not Found Error');
     }
-    ctx.id = id;
+    ctx.state.id = id;
     return next();
   },
   async getAll(ctx) {
     ctx.body = { data: 'getAll' };
   },
   async createOne(ctx) {
+    ctx.status = 201;
     ctx.body = { data: 'createOne', body: ctx.request.body };
   },
   async getOne(ctx) {
-    ctx.body = { data: 'getOne', param: ctx.id };
+    ctx.body = { data: 'getOne', param: ctx.state.id };
   },
   async updateOne(ctx) {
-    ctx.body = { data: 'updateOne', param: ctx.id, body: ctx.request.body };
+    ctx.body = { data: 'updateOne', param: ctx.state.id, body: ctx.request.body };
   },
   async deleteOne(ctx) {
-    ctx.body = { data: 'deleteOne', param: ctx.id };
+    ctx.body = { data: 'deleteOne', param: ctx.state.id };
   },
   async me(ctx) {
     ctx.body = 'me';
