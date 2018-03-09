749b57828f03a298418852057a833efb21905be0
ravendcode <ravendcode@gmail.com>
2018-03-09 20:39:17 +0200

    Add trycatch

--- src/api/user/user.handler.js
+++ src/api/user/user.handler.js
@@ -18,20 +18,24 @@ export default {
     ctx.body = { status: 'OK', message: 'getAll', data: db.users };
   },
   async createOne(ctx) {
-    const newUser = {};
-    newUser.id = db.users.length + 1;
-    newUser.username = ctx.request.body.username;
-    newUser.password = ctx.request.body.password;
-    newUser.email = ctx.request.body.email;
-    const token = await jwt.sign({ id: newUser.id }, process.env.JWT_SECRET);
-    newUser.accessToken = token;
-    if (db.users.find(userDb => userDb.username === ctx.request.body.username)) {
-      ctx.throw(400, `Username ${ctx.request.body.username} already taken`);
+    try {
+      const newUser = {};
+      newUser.id = db.users.length + 1;
+      newUser.username = ctx.request.body.username;
+      newUser.password = ctx.request.body.password;
+      newUser.email = ctx.request.body.email;
+      const token = await jwt.sign({ id: newUser.id }, process.env.JWT_SECRET);
+      newUser.accessToken = token;
+      if (db.users.find(userDb => userDb.username === ctx.request.body.username)) {
+        ctx.throw(400, `Username ${ctx.request.body.username} already taken`);
+      }
+      db.users.push(newUser);
+      await writeFile(config.resourcesDir + '/storage/memory/db.json', JSON.stringify(db, null, 2));
+      ctx.status = 201;
+      ctx.body = { status: 'Created', message: 'createOne', data: newUser };
+    } catch (err) {
+      ctx.throw(500, err);
     }
-    db.users.push(newUser);
-    await writeFile(config.resourcesDir + '/storage/memory/db.json', JSON.stringify(db, null, 2));
-    ctx.status = 201;
-    ctx.body = { status: 'Created', message: 'createOne', data: newUser };
   },
   async getOne(ctx) {
     ctx.body = { status: 'OK', message: 'getOne', data: ctx.state.id };
